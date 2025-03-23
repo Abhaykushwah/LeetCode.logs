351aef0443f159893fc1be4570bd2af2ecdbf792
Abhay Kushwah <67948225+Abhaykushwah@users.noreply.github.com>
2025-03-23 18:45:34 +0530

    Update power-of-four.cpp

--- 342-power-of-four/power-of-four.cpp
+++ 342-power-of-four/power-of-four.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     bool isPowerOfFour(int n) {
+    // Bitwise Method (Efficient):
+    // Check if n is a power of 2 (n & (n-1) == 0).
+    // Ensure that the only set bit is in an odd position using n % 3 == 1
         return (n > 0) && ((n & (n - 1)) == 0) && (n % 3 == 1);
     }
 };
