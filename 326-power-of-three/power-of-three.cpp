5aa5efedc01916e83044fd539748365cd3b42544
Abhay Kushwah <67948225+Abhaykushwah@users.noreply.github.com>
2025-03-23 18:02:07 +0530

    Time: 4 ms (48.62%) | Memory: 8.7 MB (97.21%) - LeetSync

--- 326-power-of-three/power-of-three.cpp
+++ 326-power-of-three/power-of-three.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     bool isPowerOfThree(int n) {
-        while(n>0 and n%3 == 0){
-            n = n/3;
-        }
-        return n ==1;
+        // while(n>0 and n%3 == 0){
+        //     n = n/3;
+        // }
+        // return n ==1;
         
-        // return (n>0) and (n*n*n % n == 0);
+        return (n>0) and (1162261467 % n == 0);
     }
 };
