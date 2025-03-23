11d7d3aeef8be8ac854671d9aff96d4734b09a64
Abhay Kushwah <67948225+Abhaykushwah@users.noreply.github.com>
2025-03-23 18:04:22 +0530

    Update power-of-three.cpp

--- 326-power-of-three/power-of-three.cpp
+++ 326-power-of-three/power-of-three.cpp
@@ -1,11 +1,12 @@
 class Solution {
 public:
     bool isPowerOfThree(int n) {
-        // while(n>0 and n%3 == 0){
-        //     n = n/3;
-        // }
-        // return n ==1;
-        
-        return (n>0) and (1162261467 % n == 0);
+        // Method 1
+        while(n>0 and n%3 == 0){
+            n = n/3;
+        }
+        return n ==1;
+        // Method 2 mod to max value of int
+        // return (n>0) and (1162261467 % n == 0); 
     }
 };
